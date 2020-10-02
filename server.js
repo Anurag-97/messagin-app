9b29af9285d7075a5afc39b5fc4b8e0db0d88915
Anurag Pandey <mailanurag97@gmail.com>
2020-10-02 19:27:15 +0530

    web

--- server.js
+++ server.js
@@ -107,9 +107,9 @@ app.post('/add',(req,res)=>{
     })
 })
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html'));
+        res.sendFile(path.join(__dirname,'client/build/index.html'));
     })
 }
 app.listen(port,()=>{
